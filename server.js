e7e247c34449127ef78a3c520c98aec52a12d65f
Ramon Luan da Silva <ramonluansilva@gmail.com>
2026-06-23 02:08:33 -0300

    teste com erro

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const db = createDb('./database.db');
 const mailer = new Mailer();
 const app = createApp({ db, mailer });
 
+const ahsdah = 1;
+
 initDb(db).then(() => {
   app.listen(3000, () => {
     console.log('Rodando em http://localhost:3000');
